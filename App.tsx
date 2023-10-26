296352fe6441f694d191c6f30cfd000c8612828a
Mr-BAKH <mr.bakh.usb@gmail.com>
2023-10-26 19:32:39 +0330

    basic functionality of useing the sqlite

--- App.tsx
+++ App.tsx
@@ -1,118 +1,132 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import SQLite from 'react-native-sqlite-storage';
+import { View, Text, TouchableOpacity, TextInput, Alert, StatusBar } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const db = SQLite.openDatabase({
+  name: 'mydb',
+  location: 'default'
+},
+() => {
+  console.log("Database connected!")
+}, //on success
+error => console.log("Database error", error) //on error
+)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
+const App = ()=> {
+
+  useEffect(() => {
+    createUserTable(); //call create table function here
+  },[])
+
+  //create table function
+  const createUserTable = () => {
+      db.executeSql(
+        "CREATE TABLE IF NOT EXISTS "+
+        "users "+
+        "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR, name VARCHAR)",
+        [],
+        (result) => {
+          console.log("Table created successfully");
+        }, (error) => {
+          console.log("Create table error", error)
+        })
+  }
+
+  const createUser = async() => {
+    let sql = "INSERT INTO users (email, name) VALUES (?, ?)";
+    let params = ["yoursocialmd@gmail.com", "MD Sarfaraj"]; //storing user data in an array
+    await db.executeSql(sql, params, (result) => {
+        Alert.alert("Success", "User created successfully.");
+    }, (error) => {
+        console.log("Create user error", error);
+    });
+}
+
+const listUsers = async () => {
+  let sql = "SELECT * FROM users";
+  db.transaction((tx) => {
+      tx.executeSql(sql, [], (tx, resultSet) => {
+          var length = resultSet.rows.length;
+          for (var i = 0; i < length; i++) {
+              console.log(resultSet.rows.item(i));
+          }
+      }, (error) => {
+          console.log("List user error", error);
+      })
+  })
+}
+
+const updateUser = () => {
+  let sql = 'UPDATE users SET email = ?, name = ? WHERE id = ?';
+  let params = ['yoursocialmd@gmail.com', "Mohammad Sarfaraj", 1];
+  db.executeSql(sql, params,(resultSet) => {
+      Alert.alert("Success", "Record updated successfully");
+  }, (error) => {
+      console.log(error);
+  });
+}
+
+const deleteUser = () => {
+  let sql = "DELETE FROM users WHERE id = ?";
+  let params = [1];
+  db.executeSql(sql, params, (resultSet) => {
+      Alert.alert("Success", "User deleted successfully");
+  }, (error) => {
+      console.log("Delete user error", error);
+  })
+}
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View
+      style={{
+        flex:1,
+        alignItems:'center',
+        gap:10
+      }}
+    >
+      <Text>
+        test SQLite
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <TouchableOpacity
+        onPress={createUser}
+        style={{backgroundColor:'blue',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      >
+        <Text>
+          Create User
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={listUsers}
+        style={{backgroundColor:'green',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      >
+        <Text>
+          showUser
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={updateUser}
+        style={{backgroundColor:'yellow',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      >
+        <Text>
+          Update
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={deleteUser}
+        style={{backgroundColor:'red',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      >
+        <Text>
+          Delete
+        </Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
