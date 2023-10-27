621409b48f08be9edcb75bdd35466e8713f16561
Mr-BAKH <mr.bakh.usb@gmail.com>
2023-10-28 00:36:00 +0330

    create a complicte application and use SQLite in it

--- App.tsx
+++ App.tsx
@@ -1,83 +1,148 @@
 import SQLite from 'react-native-sqlite-storage';
-import { View, Text, TouchableOpacity, TextInput, Alert, StatusBar } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Alert, StatusBar, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 
-const db = SQLite.openDatabase({
-  name: 'mydb',
-  location: 'default'
+const db =SQLite.openDatabase({
+  name:'task',
+  location:'default'
 },
-() => {
-  console.log("Database connected!")
-}, //on success
-error => console.log("Database error", error) //on error
+()=>{
+  console.log('Open task successfully!');
+},
+error=>{
+  console.log("Error: ", error)
+}
 )
-
-
 
 const App = ()=> {
 
-  useEffect(() => {
-    createUserTable(); //call create table function here
-  },[])
+  const [task, setTask]= useState<string>('')
+  const [showlist, setShowlist] = useState([])
+  const [edit, setEdit] = useState<boolean>(false)
+  const [editID, setEditID] = useState<number>()
 
-  //create table function
-  const createUserTable = () => {
-      db.executeSql(
-        "CREATE TABLE IF NOT EXISTS "+
-        "users "+
-        "(id INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR, name VARCHAR)",
-        [],
-        (result) => {
-          console.log("Table created successfully");
-        }, (error) => {
-          console.log("Create table error", error)
-        })
+  //handle creatign a table
+  const createtable = () => {
+    db.executeSql(
+      "CREATE TABLE IF NOT EXISTS "
+      +"task "
+      +"(id INTEGER PRIMARY KEY AUTOINCREMENT, task VARCHAR)",
+      [],
+      (result) => {
+        console.log("Table created successfully");
+      }, 
+      (error) => {
+        console.log("Create table error", error)
+    })
   }
 
-  const createUser = async() => {
-    let sql = "INSERT INTO users (email, name) VALUES (?, ?)";
-    let params = ["yoursocialmd@gmail.com", "MD Sarfaraj"]; //storing user data in an array
-    await db.executeSql(sql, params, (result) => {
-        Alert.alert("Success", "User created successfully.");
+  //handle add item
+  const addTask = ()=>{
+    if(task !== ''){
+      let sql = "INSERT INTO task (task) VALUES (?)";
+      let params = [task]; 
+      setTask('')
+      db.executeSql(sql, params, (result) => {
+          Alert.alert("Success", "Task created successfully.");
+      }, (error) => {
+          console.log("Create Task error", error);
+      });
+      listUsers()
+    }else{
+      Alert.alert('please Write a task!')
+    }
+  }
+
+  //fetch task
+  const listUsers = async () => {
+    let sql = "SELECT * FROM task";
+    db.transaction((tx) => {
+        tx.executeSql(sql, [], (tx, resultSet) => {
+            var length = resultSet.rows.length;
+            let task:any = []
+            for (let i = 0; i < length; i++){
+                // console.log(resultSet.rows.item(i))
+                task.push(resultSet.rows.item(i))
+            }
+            setShowlist(task);
+            // console.log(showlist)
+        }, (error) => {
+            console.log("List user error", error);
+        })
+    })
+}
+
+  //handle delete task
+  const deleteTast = (id:number) => {
+    // console.log(i)
+    let sql = "DELETE FROM task WHERE id = ?";
+    let params = [id];
+    db.executeSql(sql, params, (resultSet) => {
+        Alert.alert("Success", "Task deleted successfully!");
+        listUsers()
     }, (error) => {
-        console.log("Create user error", error);
+        console.log("Delete user error", error);
+    })
+}
+
+  //handle updating task
+  const updateUser = () => {
+    let sql = 'UPDATE task SET task = ? WHERE id = ?';
+    let params = [task, editID];
+    db.executeSql(sql, params, (resultSet) => {
+        Alert.alert("Success", "Record updated successfully");
+        setEdit(false);
+        listUsers();
+        setTask('')
+    }, (error) => {
+        console.log(error);
+        setTask('')
     });
 }
 
-const listUsers = async () => {
-  let sql = "SELECT * FROM users";
-  db.transaction((tx) => {
-      tx.executeSql(sql, [], (tx, resultSet) => {
-          var length = resultSet.rows.length;
-          for (var i = 0; i < length; i++) {
-              console.log(resultSet.rows.item(i));
+  //handle show use by Id
+  const selectUserById = (id:any) => {
+    return new Promise((resolve, reject) => {
+      db.transaction((tx) => {
+        tx.executeSql(
+          'SELECT * FROM task WHERE id = ?',
+          [id],
+          (tx, results) => {
+            if (results.rows.length === 1) {
+              // User found, resolve with user data
+              const user = results.rows.item(0);
+              resolve(user);
+            } else {
+              // User not found, resolve with null
+              resolve(null);
+            }
+          },
+          (tx, error) => {
+            reject(error);
           }
-      }, (error) => {
-          console.log("List user error", error);
-      })
-  })
-}
+        );
+      });
+    }).then((val:any) => {
+      if (val) {
+        // console.log(task)
+        setTask(val.task)
+        setEditID(val.id)
+      } else {
+        console.log('User not found');
+      }
+    })
+    .catch((error:any) => {
+      console.error('Error selecting user:', error);
+    });
+  };
 
-const updateUser = () => {
-  let sql = 'UPDATE users SET email = ?, name = ? WHERE id = ?';
-  let params = ['yoursocialmd@gmail.com', "Mohammad Sarfaraj", 1];
-  db.executeSql(sql, params,(resultSet) => {
-      Alert.alert("Success", "Record updated successfully");
-  }, (error) => {
-      console.log(error);
-  });
-}
-
-const deleteUser = () => {
-  let sql = "DELETE FROM users WHERE id = ?";
-  let params = [1];
-  db.executeSql(sql, params, (resultSet) => {
-      Alert.alert("Success", "User deleted successfully");
-  }, (error) => {
-      console.log("Delete user error", error);
-  })
-}
+  useEffect(()=>{
+    // create a table
+    createtable()
+    listUsers()
+  },[])
+  
 
   return (
     <View
@@ -87,46 +152,138 @@ const deleteUser = () => {
         gap:10
       }}
     >
-      <Text>
-        test SQLite
-      </Text>
-      <TouchableOpacity
-        onPress={createUser}
-        style={{backgroundColor:'blue',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      <StatusBar backgroundColor={'yellow'} barStyle='dark-content'/>
+      <TextInput
+        placeholder='write a task...'
+        onChangeText={setTask}
+        value={task}
+        style={{width:'100%',fontSize:20,paddingHorizontal:20,paddingVertical:20}}
+      />
+      <View
+        style={{
+          flexDirection:'row',
+          gap:5,
+          justifyContent:"center"
+        }}
       >
-        <Text>
-          Create User
-        </Text>
-      </TouchableOpacity>
+        <Button 
+          title={'Creat Task'} 
+          fuc={addTask} 
+          color={'darkgreen'}
+          edit={edit}
+          appplyFunc={updateUser}
+        />
+      </View>
 
-      <TouchableOpacity
-        onPress={listUsers}
-        style={{backgroundColor:'green',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
+      <ScrollView
+        style={{
+          width:'100%',          
+        }}
+        contentContainerStyle={{
+          paddingHorizontal:70,
+          paddingVertical:20,
+          justifyContent:'flex-start',
+          gap:10,
+        }}
+        showsVerticalScrollIndicator={false}
       >
-        <Text>
-          showUser
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={updateUser}
-        style={{backgroundColor:'yellow',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
-      >
-        <Text>
-          Update
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={deleteUser}
-        style={{backgroundColor:'red',width:'80%',margin:'auto',padding:15,alignItems:'center'}}
-      >
-        <Text>
-          Delete
-        </Text>
-      </TouchableOpacity>
+        {showlist.length > 0 ? showlist.map((item,index)=>{
+          // console.log(item.id)
+          return(
+            <Task_COM
+             key={index}
+             id={item['id']} 
+             title={item['task']} 
+             fuc={()=> console.log('task id:!')}
+             delete_fuc={deleteTast}
+             edit={edit}
+             setEdit={setEdit}
+             selectUserById={selectUserById}
+            />
+          )
+        })
+        :
+        <View
+          style={{flex:1,alignItems:'center',justifyContent:'center'}}
+        >
+          <Text>nothing to show!</Text>
+        </View>
+        }
+      </ScrollView>
+      
     </View>
   )
 }
 
 export default App;
+
+
+const Button = ({title, fuc, color, edit,appplyFunc}:any)=>{
+  return(
+    <TouchableOpacity
+      onPress={edit? appplyFunc : fuc}
+      style={{
+        backgroundColor:color,
+        padding:10,
+        paddingHorizontal:70,
+        borderRadius:10,
+      }}
+    >
+      <Text style={{color:"white"}}>
+        {edit ? 'Apply Edit': title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const Task_COM = ({title,fuc,id,delete_fuc,edit,setEdit,selectUserById}:any)=>{
+
+  return(
+    <TouchableOpacity
+      onPress={fuc}
+      style={{
+        width:'98%',
+        position:'relative',
+        padding:10,
+        borderRadius:10,
+        backgroundColor:"darkorange",
+        justifyContent:'center',
+        alignItems:'center'
+      }}
+    >
+      <Text style={{color:'white'}}>{title}</Text>
+      {/* action button */}
+      <TouchableOpacity
+        onPress={() => delete_fuc(id)}
+        style={{
+         backgroundColor:'darkred',
+          position:'absolute',
+          right:-60,
+          padding:7,
+          borderRadius:10,
+        }}
+      >
+        <Text style={{color:"white"}}>Delete</Text>
+      </TouchableOpacity>
+
+      {!edit && 
+        <TouchableOpacity
+          onPress={() => {
+            setEdit(!edit)
+            selectUserById(id)
+          }}
+          style={{
+          backgroundColor:'darkblue',
+            position:'absolute',
+            left:-50,
+            padding:7,
+            borderRadius:10,
+          }}
+        >
+          <Text style={{color:"white"}}>Edit</Text>
+        </TouchableOpacity>
+      }
+
+    </TouchableOpacity>
+  )
+}
